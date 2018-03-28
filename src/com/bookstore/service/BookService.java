c4bff43356ed5e468a9e487f2e87d045f89fe3fb
Mikhail Volkov2 <mikhail_volkov2@epam.com>
2018-03-28 13:14:03 +0300

    Рефакторинг

--- src/com/bookstore/service/BookService.java
+++ src/com/bookstore/service/BookService.java
@@ -1,16 +1,14 @@
 package com.bookstore.service;
 
 import com.bookstore.domain.Book;
-import com.bookstore.exceptions.BookNotFoundException;
-
 import java.util.List;
 
 public interface BookService {
     void insert(Book book);
 
-    void update(Book book) throws BookNotFoundException;
+    void update(Book book);
 
-    void delete(Book book) throws BookNotFoundException;
+    void delete(Book book);
 
     List<Book> getAllBooks();
 
